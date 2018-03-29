341697ca3586f6531d748e1c348950021ebc32f0
Kamil Makoś <makos.kamil@o2.pl>
2018-03-29 10:28:12 +0200

    Change_in_line_4_JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function drawTree(w) {
   var star = '';
     
-    for (var i=0; i<=w; i++) {
+    for (var i=0; i<w; i++) {
         star = '*';
         for (var j=0; j<i; j++ ) {
             star += '*';
